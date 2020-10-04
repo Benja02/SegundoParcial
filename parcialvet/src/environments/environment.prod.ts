d93cc026bd5250406ebbeba8dad96d9592c327da
Benja02 <54460514+Benja02@users.noreply.github.com>
2020-10-03 22:14:15 -0600

    login creado

---
+++ parcialvet/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  Firebase : {
+  apiKey: "AIzaSyCkBWTs8Y-RZKyYZeLGDYki8iJqWm7G9RM",
+  authDomain: "parcial2dps-vet.firebaseapp.com",
+  databaseURL: "https://parcial2dps-vet.firebaseio.com",
+  projectId: "parcial2dps-vet",
+  storageBucket: "parcial2dps-vet.appspot.com",
+  messagingSenderId: "172711774032",
+  appId: "1:172711774032:web:4f9c5f3df5670ad11ace55",
+  measurementId: "G-RE4FW3FX40"
+}
+};
---
+++ parcialvet/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  Firebase : {
+    apiKey: "AIzaSyCkBWTs8Y-RZKyYZeLGDYki8iJqWm7G9RM",
+    authDomain: "parcial2dps-vet.firebaseapp.com",
+    databaseURL: "https://parcial2dps-vet.firebaseio.com",
+    projectId: "parcial2dps-vet",
+    storageBucket: "parcial2dps-vet.appspot.com",
+    messagingSenderId: "172711774032",
+    appId: "1:172711774032:web:4f9c5f3df5670ad11ace55",
+    measurementId: "G-RE4FW3FX40"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
